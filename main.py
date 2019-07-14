cb26a16c5bf48ecf94475fa8d0a822efdb74425d
Shereeba Pruitt <pruitt.shereeba@gmail.com>
2019-07-14 15:37:07 -0500

    Initial database for award winners added, calex

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 from flask import Flask, request, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://pkd-award:password@localhost:8889/pkd-award"
+app.config["SQLALCHEMY_ECHO"] = True
+
+db = SQLAlchemy(app)
+
+class Winners(db.Model):
+    id = db.Column(db.Integer, Primary_key = True)
+    year = db.Column(db.Integer)
+    first_name = db.Column(db.String(120))
+    last_name = db.Column(db.String(120))
+    book_title = db.Column(db.String(120))
+
+    def __init__(self, year, first_name, last_name, book_title):
+        self.year = year
+        self.first_name = first_name
+        self.last_name = last_name
+        self.book_title = book_title
 
 @app.route("/")
 def index():
     return "Somnacode presents: The Philip K Dick Award APP"
 
-app.run()
+if __name__ == "__main__":
+    app.run()
