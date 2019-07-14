2f2747f36f59c412e64c94a6d2faf47fc7640083
Shereeba Pruitt <pruitt.shereeba@gmail.com>
2019-07-14 16:39:45 -0500

    Added Database info for award winners, 1982 - 2018, calex

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -10,7 +10,7 @@ app.config["SQLALCHEMY_ECHO"] = True
 db = SQLAlchemy(app)
 
 class Winners(db.Model):
-    id = db.Column(db.Integer, Primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     year = db.Column(db.Integer)
     first_name = db.Column(db.String(120))
     last_name = db.Column(db.String(120))
@@ -25,6 +25,21 @@ class Winners(db.Model):
 @app.route("/")
 def index():
     return "Somnacode presents: The Philip K Dick Award APP"
+
+@app.route("/update", methods=["POST", "GET"])
+def award_update():
+    if request.method == "POST":
+        year = request.form["year"]
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
+        book_title = request.form["book_title"]
+
+        new_data = Winners(year, first_name, last_name, book_title)
+
+        db.session.add(new_data)
+        db.session.commit()
+
+    return render_template("update.html")
 
 if __name__ == "__main__":
     app.run()
